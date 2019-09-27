15d61ec8a76e6292e441b927b9578673fd1ec492
Ruth Newman <ruthnewman@ruths-mbp.home>
2019-09-27 15:27:38 +0100

    passing third test

--- src/Game.php
+++ src/Game.php
@@ -15,8 +15,15 @@ class Game
     public function score()
     {
         $score = 0;
-        for ($i = 0; $i < count($this->rolls); $i++) {
-            $score += $this->rolls[$i];
+        $i = 0;
+        for ($frame = 0; $frame < 10; $frame++) {
+            if ($this->rolls[$i] + $this->rolls[$i + 1] === 10) {
+                $score += 10 + $this->rolls[$i + 2];
+                $i += 2;
+            } else {
+                $score += $this->rolls[$i] + $this->rolls[$i + 1];
+                $i += 2;
+            }
         }
         return $score;
     }
--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -30,14 +30,14 @@ class BowlingGameTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(20, $this->game->score());
     }
 
-    // public function testOneSpare()
-    // {
-    //     $this->game->roll(5);
-    //     $this->game->roll(5);
-    //     $this->game->roll(3);
-    //     $this->rollMany(17,0);
-    //     $this->assertEquals(16, $this->game->score());
-    // }
+    public function testOneSpare()
+    {
+        $this->game->roll(5);
+        $this->game->roll(5);
+        $this->game->roll(3);
+        $this->rollMany(17,0);
+        $this->assertEquals(16, $this->game->score());
+    }
 
 
 }
